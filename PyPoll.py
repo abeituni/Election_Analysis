929038c9ff02db7899b7b00f17e91af91815fd53
Amir B <amirb@Amirs-MacBook-Air-2.local>
2021-09-03 19:21:08 -0700

    Adding Pypoll.py file

--- PyPoll.py
+++ PyPoll.py
@@ -5,11 +5,6 @@
 #4 The total number of votes each candidate won
 #5 The winner of the election based on popular vote
 #Importing Depedencies(?)----
-import csv
-import os
-#Command Line
-#assign a variable to load a file from a path
-file_to_load = os.path.join("resources","election_results.csv")
 # Import the datetime class from the datetime module.
 import datetime as dt
 # Use the now() attribute on the datetime class to get the present time.
@@ -18,14 +13,30 @@ now = dt.datetime.now()
 print("The time right now is ", now)
 import random
 import numbers
+
+#Add our dependenies
+import csv
+import os
+#assign a variable to load a file from a path
+file_to_load = os.path.join("resources","election_results.csv")
 #Create a filename variable to direct or inderect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
-#Using the open() function with the "w" mode we will write data to the file
-open(file_to_save, "w")
+
 #Open the election results and read the file.
 with open(file_to_load) as election_data:
-    #To do: perform analysis
-    print(election_data)
+    #read the file object with the reader function
+    file_reader = csv.reader(election_data)
+    #Print the header row.
+    headers = next(file_reader)
+    print(headers)
+
+#Using the open() function with the "w" mode we will write data to the file
+with open(file_to_save, "w") as txt_file:
+    #Write three counties to the file
+    txt_file.write("Counties in the Election\n----------------------\n")
+    txt_file.write("Arapahoe\nDenver\nJefferson")
+
+
 
 
 
